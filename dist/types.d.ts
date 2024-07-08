c8acacc6d33ef99d975be5dac5c7bfaa434baab0
oluwaferanmi <feranmi.com>
2024-07-08 15:39:49 +0100

    aaa

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,2 +1,12 @@
+export interface VerificationComponentProps {
+    onVerificationCompleted: (verificationReference: string) => void;
+    onVerificationFailed: (error: string) => void;
+    onVerificationSubmitted: (verificationReference: string) => void;
+    onVerificationCanceled: () => void; //exited
+    verificationType: VerificationTypes["image"] | VerificationTypes["video"];
+}
 
-export {  }
+export interface VerificationTypes {
+    image: string;
+    video: string;
+}
